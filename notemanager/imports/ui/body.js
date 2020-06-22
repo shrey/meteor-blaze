a7be1513b5973d62f35a99c2db0aee25215d86bb
Shrey Dubey <wshrey09@gmail.com>
2020-06-22 19:32:33 +0530

    Built it further

--- notemanager/imports/ui/body.js
+++ notemanager/imports/ui/body.js
@@ -1,11 +1,32 @@
 import { Template } from 'meteor/templating';
-
+import {ReactiveDict} from 'meteor/reactive-dict';
 import './body.html';
 
+import {Tasks} from '../api/tasks';
+
+import './task'
+
+Template.body.onCreated(function bodyOnCreated(){
+    this.state = new ReactiveDict();
+})
+
 Template.body.helpers({
-  tasks: [
-    {text: 'Task 1'},
-    {text: 'Task 2'},
-    {text: 'Task 3'}
-  ]
+  tasks(){
+      return Tasks.find({}, {sort: {createdAt: -1}});
+  }
+})
+
+Template.body.events({
+    'submit .new-task'(event) {
+        event.preventDefault();
+        console.log(event.target.text.value);
+        const target = event.target;
+        const text = target.text.value;
+
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+        })
+        target.text.value = '';
+    }
 })
